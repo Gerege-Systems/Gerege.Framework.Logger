980d9f6d030bda005acc1a469e84cee3645d8ada
Наранхүү <codesaur@gmail.com>
2024-02-15 14:05:11 +0800

    using .NET 8 now

---
+++ LoggerExample/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace LoggerExample
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+
+            var db = new ConsoleLogger();
+            db.Connect("my-imaginary-database");
+            db.Trace("general", "Program button clicked", new { button = myButton.Name });
+
+            myButton.Content += " - Logged";
+        }
+    }
+}
